ed20b9bcf548a34c11520e8abcc56f46babb0d51
NESMA47 <nesmaeid422@gmail.com>
2026-03-02 21:20:11 +0200

    feat: complete login  component logic, styling, and validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Signup} from './feature/auth/signup/signup';
+import { Login } from './feature/auth/login/login';
+export const routes: Routes = [
+  { path: 'signup', component: Signup },
+  { path: 'login', component: Login}, 
+  { path: '', redirectTo: 'signup', pathMatch: 'full' }
+];
