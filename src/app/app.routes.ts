0d85ed4dcf3c14ee61ba38ceac861f27d16fae88
NESMA47 <nesmaeid422@gmail.com>
2026-03-03 17:06:46 +0200

    fix: apply reviewer feedback, use bootstrap utilities and fix placeholder visibility

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { Signup} from './feature/auth/signup/signup';
-import { Login } from './feature/auth/login/login';
-export const routes: Routes = [
-  { path: 'signup', component: Signup },
-  { path: 'login', component: Login}, 
-  { path: '', redirectTo: 'signup', pathMatch: 'full' }
-];
+
+export const routes: Routes = [];
